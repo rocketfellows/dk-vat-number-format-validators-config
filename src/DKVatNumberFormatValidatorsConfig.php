d53b39b26961ad20890c60e0d80be48bec83af56
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-05-07 20:58:04 +0400

    [CVFV-52] setup config.

--- src/DKVatNumberFormatValidatorsConfig.php
+++ src/DKVatNumberFormatValidatorsConfig.php
@@ -1,21 +1,22 @@
 <?php
 
-// TODO: namespace
+namespace rocketfellows\DKVatNumberFormatValidatorsConfig;
 
 use arslanimamutdinov\ISOStandard3166\Country;
 use arslanimamutdinov\ISOStandard3166\ISO3166;
 use rocketfellows\CountryVatFormatValidatorInterface\CountryVatFormatValidatorInterface;
+use rocketfellows\DKVatFormatValidator\DKVatFormatValidator;
 use rocketfellows\SpecificCountryVatNumberFormatValidatorsConfig\SpecificCountryVatNumberFormatValidatorsConfig;
 
-class CountryVatNumberFormatValidatorsConfig extends SpecificCountryVatNumberFormatValidatorsConfig
+class DKVatNumberFormatValidatorsConfig extends SpecificCountryVatNumberFormatValidatorsConfig
 {
     public function getCountry(): Country
     {
-        return ISO3166::COUNTRY();
+        return ISO3166::DK();
     }
 
     protected function getDefaultValidator(): CountryVatFormatValidatorInterface
     {
-        return new CountryVatFormatValidator();
+        return new DKVatFormatValidator();
     }
 }
